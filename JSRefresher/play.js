001cba6ce54e51a9e3ccdfe5f98f24fcd199ce53
truashamu <altriatree@gmail.com>
2022-05-08 08:53:45 -0500

    -

--- JSRefresher/play.js
+++ JSRefresher/play.js
@@ -1,11 +1,23 @@
-var name = 'Party Poison';
-var age = 1e9+7;
-var isCool = true;
+const killjoy = {
+    name: 'Party Poison',
+    age: 1e9+7,
+    isCool: true,
+    title () {
+        console.log('Hi, I am ' + this.name);
+    }
+}
+/* 
+let name = 'Party Poison';
+let age = 1e9+7;
+let isCool = true;
+*/
 
-function summarizeUser(userName, userAge, userCool) {
+const summarizeUser = (userName, userAge, userCool) => {
     return (' Name: ' + userName + 
             ' // Age: ' + userAge + 
             ' // isCool: ' + userCool); 
-}
+};
 
-console.log(summarizeUser(name, age, isCool));
+//console.log(summarizeUser(name, age, isCool));
+console.log(killjoy);
+killjoy.title();
