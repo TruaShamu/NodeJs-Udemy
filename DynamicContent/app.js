82b31e4d55da655d54720c26df2102059cd7a1a4
Trua Shamu <sofia_mit@hotmail.com>
2022-06-19 18:31:11 -0500

    a

--- DynamicContent/app.js
+++ DynamicContent/app.js
@@ -6,10 +6,7 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// app.engine('hbs', expressHbs.engine());
-app.engine('hbs', expressHbs.engine({extname: "hbs", layoutsDir: "views/layouts/", defaultLayout: "main-layout"}));
-
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -22,6 +19,6 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+    res.status(404).render('404', {pageTitle: 'Page Not Found', path: 'Error'});
 });
 app.listen(3000);
