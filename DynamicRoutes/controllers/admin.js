4a35ea1b351f63b6cfb325cbbffd91ba401a51f3
Trua Shamu <sofia_mit@hotmail.com>
2022-06-25 15:16:55 -0500

    s

--- DynamicRoutes/controllers/admin.js
+++ DynamicRoutes/controllers/admin.js
@@ -1,12 +1,11 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+  res.render('admin/edit-product', {
+	pageTitle: 'Add Product',
+	path: '/admin/add-product',
+	editing: false
   });
 };
 
@@ -15,17 +14,55 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  console.log(editMode);
+  if (!editMode) {
+	  return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+	if (!product) {
+		return res.redirect('/')
+	}
+	res.render('admin/edit-product', {
+	  pageTitle: 'Add Product',
+	  path: '/admin/add-product',
+	  editing: editMode,
+	  product: product
+	  });
+  });
+  
+};
+
+exports.postEditProduct = (req, res, next) => {
+	console.log("Got to post-edit");
+	const prodId = req.body.productId;
+	const updatedTitle = req.body.title;
+	const updatedImageUrl = req.body.imageUrl;
+	const updatedPrice = req.body.price;
+	const updatedDesc = req.body.description;
+	const updatedProduct = new Product(
+		prodId,
+		updatedTitle,
+		updatedImageUrl,
+		updatedDesc,
+		updatedPrice);
+	console.log(updatedProduct);
+	updatedProduct.save();
+	res.redirect('/admin/products');
+}
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
+	res.render('admin/products', {
+	  prods: products,
+	  pageTitle: 'Admin Products',
+	  path: '/admin/products'
+	});
   });
 };
--- DynamicRoutes/controllers/shop.js
+++ DynamicRoutes/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
 	Product.fetchAll(products => {
@@ -34,6 +35,9 @@ exports.getIndex = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
 	const prodId = req.body.productId;
+	Product.findById(prodId, (product) => {
+		Cart.addProduct(prodId, product.price);
+	});
 	console.log("ProdId: "+ prodId);
 	res.redirect('/cart')
 }
