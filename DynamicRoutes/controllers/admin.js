52a822fa20cc0f95cb9f2c4b970785284647b11b
Trua Shamu <sofia_mit@hotmail.com>
2022-07-05 14:51:28 -0500

    l

--- DynamicRoutes/controllers/admin.js
+++ DynamicRoutes/controllers/admin.js
@@ -10,48 +10,48 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  Product.create({
-	title: title,
-	price: price,
-	imageUrl: imageUrl,
-	description: description
-  }).
-  	then(result => {
-		console.log("Created product.");
-		res.redirect('/products');
-  	})
-  	.catch(err => {
+	const title = req.body.title;
+	const imageUrl = req.body.imageUrl;
+	const price = req.body.price;
+	const description = req.body.description;
+	req.user
+	  .createProduct({
+		title: title,
+		price: price,
+		imageUrl: imageUrl,
+		description: description
+	  })
+	  .then(result => {
+		// console.log(result);
+		console.log('Created Product');
+		res.redirect('/admin/products');
+	  })
+	  .catch(err => {
 		console.log(err);
-	});
-  
-};
+	  });
+  };
 
 exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-  console.log(editMode);
-  if (!editMode) {
-	  return res.redirect('/');
-  }
-  const prodId = req.params.productId;
-
-  Product.findByPk(prodId)
-	.then(product => {
-		if (!product) {
-			return res.redirect('/')
-		}
-		res.render('admin/edit-product', {
-		  pageTitle: 'Add Product',
-		  path: '/admin/add-product',
-		  editing: editMode,
-		  product: product
-		  });
-	})
-	.catch( err => console.log(err));
-  
+	const editMode = req.query.edit;
+	console.log(editMode);
+	if (!editMode) {
+		return res.redirect('/');
+	}
+  	const prodId = req.params.productId;
+  	req.user.getProducts({where: {id: prodId}})
+		.then(products => {
+			const product = products[0];
+			if (!product) {
+				return res.redirect('/')
+			}
+			res.render('admin/edit-product', {
+			pageTitle: 'Add Product',
+			path: '/admin/add-product',
+			editing: editMode,
+			product: product
+			});
+		})
+		.catch( err => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -77,7 +77,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+  	.getProducts()
   	.then(products => {
 		res.render('admin/products', {
 	  		prods: products,
