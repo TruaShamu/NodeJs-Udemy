c09890171b3351063726a9fcf1f3c022630fd0c2
Trua Shamu <sofia_mit@hotmail.com>
2022-07-08 18:14:23 -0500

    a

---
+++ Mongoose/util/database.js
@@ -0,0 +1,28 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+	MongoClient.connect('mongodb+srv://Trua:***REMOVED***@cluster0.uzeyt.mongodb.net/?retryWrites=true&w=majority')
+	.then(client => {
+		console.log("CONNECTED!");
+		_db = client.db();
+		callback();
+		//console.log(client);
+	})
+	.catch(err => {
+		console.log(err);
+		throw error;
+	});
+};
+
+const getDb = () => {
+	if (_db) {
+		return _db;
+	}
+	throw "No Database Found.";
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
