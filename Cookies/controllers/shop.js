a1e31d1b0b682221aeb5441265c323055051638c
Trua Shamu <sofia_mit@hotmail.com>
2022-07-11 19:15:05 -0500

    cookies

--- Cookies/controllers/shop.js
+++ Cookies/controllers/shop.js
@@ -2,10 +2,11 @@ const Product = require('../models/product');
 const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
+  console.log("get products");
   Product.find()
     .then(products => {
-      console.log(products);
       res.render('shop/product-list', {
+        isAuthenticated: req.isLoggedIn,
         prods: products,
         pageTitle: 'All Products',
         path: '/products'
@@ -23,19 +24,23 @@ exports.getProduct = (req, res, next) => {
       res.render('shop/product-detail', {
         product: product,
         pageTitle: product.title,
-        path: '/products'
+        path: '/products',
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
+  console.log("get index");
+  console.log("is authenticated" + req.isLoggedIn);
   Product.find()
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => {
@@ -51,7 +56,8 @@ exports.getCart = (req, res, next) => {
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
-        products: products
+        products: products,
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
@@ -110,7 +116,8 @@ exports.getOrders = (req, res, next) => {
       res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders',
-        orders: orders
+        orders: orders,
+        isAuthenticated: req.isLoggedIn
       });
     })
     .catch(err => console.log(err));
