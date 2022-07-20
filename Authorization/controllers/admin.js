53f392793039e34a36e15a687a0cd784ccaf6ddf
Trua Shamu <sofia_mit@hotmail.com>
2022-07-20 08:04:11 -0500

    a

--- Authorization/controllers/admin.js
+++ Authorization/controllers/admin.js
@@ -71,7 +71,6 @@ exports.postEditProduct = (req, res, next) => {
       product.imageUrl = updatedImageUrl;
       return product.save()
       .then(result => {
-      console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     });
     })
