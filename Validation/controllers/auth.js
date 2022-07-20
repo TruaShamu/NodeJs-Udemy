b6122eaf34d22d4ca7a2b74613d29aff37b98fa7
Trua Shamu <sofia_mit@hotmail.com>
2022-07-20 10:29:13 -0500

    userinput

--- Validation/controllers/auth.js
+++ Validation/controllers/auth.js
@@ -73,7 +73,8 @@ exports.getSignup = (req, res, next) => {
     path: '/signup',
     pageTitle: 'Signup',
     isAuthenticated: false,
-    errorMessage : message
+    errorMessage : message,
+    oldInput: {email: '', password: '', confirmPassword: ''}
   });
 };
 
@@ -88,7 +89,8 @@ exports.postSignup = (req, res, next) => {
       .render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        errorMessage : errors.array()[0].msg
+        errorMessage : errors.array()[0].msg,
+        oldInput: {email: email, password: password, confirmPassword: req.body.confirmPassword}
       });
   }
   bcrypt
